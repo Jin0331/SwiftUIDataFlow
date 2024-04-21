596975703417f409025a41a2ec3c9340a25ab2b7
Jin0331 <sempre813@naver.com>
2024-04-21 16:52:42 +0900

    Initial Commit

---
+++ SwiftUIDataFlow/SwiftUIDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDataFlowApp.swift
+//  SwiftUIDataFlow
+//
+//  Created by JinwooLee on 4/21/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
